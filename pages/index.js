79f5532201285711b151f8ffabd88e1a318750cf
fujiansheng <15221448373@163.com>
2022-05-02 15:08:55 +0800

    feat: create

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,38 @@
-import Layout from '@/components/Layout';
-import Grid from '@/components/Grid';
+import Layout from "@/components/Layout";
+import Grid from "@/components/Grid";
+// Import the generated Prisma client
+import { PrismaClient } from "@prisma/client";
 
-import homes from 'data.json';
+// Instantiate it
+const prisma = new PrismaClient();
 
-export default function Home() {
+// import homes from "data.json";
+
+export default function Home(props) {
+  console.log("Home props", props);
+  const { homes } = props;
+
   return (
     <Layout>
-      <h1 className="text-xl font-medium text-gray-800">
-        Top-rated places to stay
-      </h1>
-      <p className="text-gray-500">
-        Explore some of the best places in the world
-      </p>
+      <h1 className="text-xl font-medium text-gray-800">Top-rated places to stay</h1>
+      <p className="text-gray-500">Explore some of the best places in the world</p>
       <div className="mt-8">
         <Grid homes={homes} />
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  // Get all homes
+  const homes = await prisma.home.findMany();
+
+  console.log("getServerSideProps homes", homes);
+
+  // Pass the data to the Home page
+  return {
+    props: {
+      homes: JSON.parse(JSON.stringify(homes)),
+    },
+  };
 }
